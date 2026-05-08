c1f0e827badc5146a025fd4d94ef636c93a98a32
AlyssaMulia <alyssadwiana2005@gmail.com>
2026-05-08 15:49:27 +0800

    Style: Update logo  and fix code indentation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,20 +4,17 @@ export default function Footer() {
   return (
     <footer className="bg-white text-black border-t border-gray-300">
       <div className="container mx-auto px-24 py-8">
-
         <div className="flex flex-col md:flex-row justify-between items-start gap-8">
           <div className="max-w-md">
-            <img
-              src={logoImg} alt="Koding Next Logo" className="h-10 mb-4"
-            />
+            <img src={logoImg} alt="Koding Next Logo" className="h-10 mb-4" />
             <p className="text-sm leading-relaxed">
-              Jl. Imam Bonjol No.02, Pelabuhan, Kec. Samarinda Kota,<br />
+              Jl. Imam Bonjol No.02, Pelabuhan, Kec. Samarinda Kota,
+              <br />
               Kota Samarinda, Kalimantan Timur
             </p>
           </div>
 
           <div className="text-sm space-y-3">
-            
             <div>
               <p className="font-semibold">Jam Operasional:</p>
               <p>Senin – Minggu | 10:00 – 18:00 WITA</p>
@@ -50,20 +47,19 @@ export default function Footer() {
                 xmlns="http://www.w3.org/2000/svg"
                 className="w-5 h-5 text-primary-pink"
                 fill="currentColor"
-                viewBox="0 0 24 24"
+                viewBox="0 0 448 512"
               >
-                <path d="M12 2a10 10 0 00-8.94 14.47L2 22l5.7-1.49A10 10 0 1012 2zm0 18a7.96 7.96 0 01-4.06-1.11l-.29-.17-3.38.88.9-3.29-.19-.34A8 8 0 1112 20zm4.25-5.72c-.23-.11-1.37-.67-1.58-.75-.21-.08-.36-.11-.51.11s-.59.75-.72.9c-.13.15-.26.17-.49.06a6.48 6.48 0 01-1.91-1.18 7.24 7.24 0 01-1.34-1.66c-.14-.23-.02-.35.1-.46.11-.11.23-.26.34-.39.11-.13.15-.23.23-.38.08-.15.04-.28-.02-.39-.06-.11-.51-1.23-.7-1.68-.18-.44-.36-.38-.51-.39h-.44c-.15 0-.39.06-.6.28s-.78.76-.78 1.85.8 2.15.91 2.3c.11.15 1.57 2.4 3.8 3.36.53.23.95.36 1.27.46.53.17 1.02.15 1.4.09.43-.06 1.37-.56 1.56-1.1.19-.54.19-1 .13-1.1-.06-.1-.21-.17-.44-.28z" />
+                <path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7 .9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z" />
               </svg>
               <span>+62 8111-5525-959</span>
             </a>
-
           </div>
         </div>
 
         <div className="text-center text-xs text-gray-500 mt-8">
-          © {new Date().getFullYear()} - KODING NEXT SAMARINDA. ALL RIGHTS RESERVED
+          © {new Date().getFullYear()} - KODING NEXT SAMARINDA. ALL RIGHTS
+          RESERVED
         </div>
-
       </div>
     </footer>
   );
