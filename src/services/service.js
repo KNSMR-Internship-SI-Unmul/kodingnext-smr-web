aeda8860df31f39c91c20c081847b64758dafbc7
AlyssaMulia <alyssadwiana2005@gmail.com>
2026-05-08 17:53:19 +0800

    feat: update parameters to match API specifications

--- src/services/service.js
+++ src/services/service.js
@@ -3,18 +3,27 @@ import { apiClient } from "./api";
 export const service = {
   getCourses: () => apiClient("/courses", { method: "GET" }),
   getCourseById: (idOrSlug) => apiClient(`/courses/${idOrSlug}`, { method: "GET" }),
+  
   getModules: (courseTypeId = null) => {
     const endpoint = courseTypeId
       ? `/modules?course_type_id=${courseTypeId}`
       : "/modules";
     return apiClient(endpoint, { method: "GET" });
   },
-  getProjects: (courseTypeId = null) => {
-    const endpoint = courseTypeId
-      ? `/student-projects?course_type_id=${courseTypeId}`
-      : "/student-projects";
+
+  getProjects: (filters = {}) => {
+    const { search, module_id } = filters;
+    const params = new URLSearchParams();
+
+    if (search) params.append("search", search);
+    if (module_id) params.append("module_id", module_id);
+
+    const queryString = params.toString();
+    const endpoint = queryString ? `/student-projects?${queryString}` : "/student-projects";
+
     return apiClient(endpoint, { method: "GET" });
   },
+
   getPromotions: () => apiClient("/promotions", { method: "GET" }),
   getReviews: () => apiClient("/reviews", { method: "GET" }),
   getEvents: () => apiClient("/events", { method: "GET" }),
